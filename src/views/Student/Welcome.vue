973b847c913fbcc60cac63457637ff6d6579cb74
Md Feroj Bepari <feroj21@gmail.com>
2021-07-05 15:05:06 +0600

    Implement welcome API in the vuejs including login

---
+++ src/views/Student/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="content" v-html="content.text"></div>
+</template>
+<script>
+import UserDashboard from "/src/services/resources/UserDashboard";
+export default {
+  name: "Form",
+  data() {
+    return {
+      content: null
+    }
+  },
+  mounted() {
+      this.getWelcome();
+  },
+  methods: {
+    getWelcome() {
+      UserDashboard.studentDashboard().then((response) => {
+          this.content = response.data.data;
+      })
+    }
+  }
+};
+</script>
